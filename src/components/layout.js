6351dc9b95f6f8122f7f535f456ffcc85164750f
panaretos34 <panaretos34@gmail.com>
2020-10-20 10:36:00 +0300

    commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Logo from "./Logo"
+import Navigationbar from './navigationbar'
 import Footer from './Footer'
 import './layout.css'
 const layout = ({children})=> {
